c6d718813ddef8f87eff1c7e33178dd927af0e5f
proximaCent <sabikaftahee01@gmail.com>
2024-05-17 17:37:27 +0600

    first commit

---
+++ Multithread.java
@@ -0,0 +1,26 @@
+class experiment extends Thread {
+	public void run()
+	{
+		try {
+			System.out.println(
+				"Thread " + Thread.currentThread().threadId()+ " is running");
+		}
+		catch (Exception e) {
+			System.out.println("Exception is caught");
+		}
+	}
+}git add 
+
+
+public class Multithread {
+	public static void main(String[] args) throws InterruptedException
+	{
+		 
+		for (int i = 0; i < 10; i++) 
+		{
+			experiment object = new experiment();
+			object.start();
+			Thread.sleep(1000);
+		}
+	}
+}
